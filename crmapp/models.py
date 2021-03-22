db5cf4601c83f2279dbbbefe65d7b5598f086b28
pekshevds <pekshev.ds@gmail.com>
2021-03-22 22:23:21 +0300

    20210322_001 Технический долг

--- crmapp/models.py
+++ crmapp/models.py
@@ -56,25 +56,66 @@ class Person(models.Model):
 
 class PersonForm(ModelForm):
 
-	id = forms.IntegerField(widget=forms.TextInput(attrs={
-												'type': 'hidden',
-												'class': 'form-control',
-												}))
-	first_name = forms.CharField(max_length=255)
-	middle_name = forms.CharField(max_length=255, required=False)
-	last_name = forms.CharField(max_length=1024, required=False)
-	birthdate = forms.DateField(required=False)#, attrs={'type': 'date'}
-	sex = forms.CharField(max_length=1, widget=forms.Select(choices=SEX))
+	id = forms.IntegerField(widget=forms.HiddenInput(attrs={
+		'class': 'form-control',
+	}), initial=0)
 
-	email = forms.EmailField(max_length=254, required=False)
+	first_name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Имя',
+	}))
 
-	address1 = forms.CharField(max_length=1024, required=False)
-	address2 = forms.CharField(max_length=1024, required=False)
+	middle_name = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Отчество',
+	}))
 
-	phone1 = forms.CharField(max_length=25, required=False)
-	phone2 = forms.CharField(max_length=25, required=False)
+	last_name = forms.CharField(max_length=1024, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Фамилия',
+	}))
 
-	description = forms.CharField(max_length=1024, required=False)
+	birthdate = forms.DateField(required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'type': 'date',
+		'placeholder': 'Дата рождения',
+	}))
+
+	sex = forms.CharField(max_length=1, widget=forms.Select(choices=SEX, attrs={
+		'class': 'form-control',
+		'placeholder': 'Пол',
+	}), initial=DEFAULT_SEX)
+
+	email = forms.EmailField(max_length=254, required=False, widget=forms.EmailInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'E-mail',
+	}))
+
+	address1 = forms.CharField(max_length=1024, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Адрес',
+	}))
+
+	address2 = forms.CharField(max_length=1024, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Адрес',
+	}))
+
+	phone1 = forms.CharField(max_length=25, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Телефон',
+		}))
+
+	phone2 = forms.CharField(max_length=25, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Телефон',
+	}))
+
+	description = forms.CharField(max_length=1024, required=False, widget=forms.Textarea(attrs={
+		'class': 'form-control',
+		'rows': '5',
+		'placeholder': 'Описание',
+	}))
 
 	class Meta:
 		model = Person
@@ -82,6 +123,7 @@ class PersonForm(ModelForm):
 			'id',
 			'first_name',
 			'middle_name',
+			'last_name',
 			'birthdate',
 			'sex',
 			'email',
@@ -92,9 +134,7 @@ class PersonForm(ModelForm):
 			'description',
 			]
 
-		widgets = {
-			'description': forms.Textarea(),			
-		}
+		
 
 
 class Customer(models.Model):
@@ -132,6 +172,80 @@ class Customer(models.Model):
 		verbose_name = 'Заказчик'
 		verbose_name_plural = 'Заказчики'
 		ordering = ['name']
+
+
+class CustomerForm(ModelForm):
+
+	id = forms.IntegerField(widget=forms.HiddenInput(attrs={
+		'class': 'form-control',
+	}), initial=0)
+
+	name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Наименование',
+	}))
+
+	full_name = forms.CharField(max_length=1024, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Наименование полное',
+	}))
+
+	inn = forms.CharField(max_length=12, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'ИНН',
+	}))
+
+	kpp = forms.CharField(max_length=9, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'КПП',
+	}))
+	
+	email = forms.EmailField(max_length=254, required=False, widget=forms.EmailInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'E-mail',
+	}))
+
+	address1 = forms.CharField(max_length=1024, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Адрес',
+	}))
+
+	address2 = forms.CharField(max_length=1024, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Адрес',
+	}))
+
+	phone1 = forms.CharField(max_length=25, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Телефон',
+		}))
+
+	phone2 = forms.CharField(max_length=25, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Телефон',
+	}))
+
+	description = forms.CharField(max_length=1024, required=False, widget=forms.Textarea(attrs={
+		'class': 'form-control',
+		'rows': '5',
+		'placeholder': 'Описание',
+	}))
+
+	class Meta:
+		model = Customer
+		fields = [
+			'id',
+			'name',
+			'full_name',
+			'inn',
+			'kpp',			
+			'email',
+			'address1',
+			'address2',
+			'phone1',
+			'phone2',
+			'description',
+			]
 
 
 class Position(models.Model):
@@ -177,6 +291,81 @@ class Employee(models.Model):
 		ordering = ['name']
 
 
+class EmployeeForm(ModelForm):
+
+	id = forms.IntegerField(widget=forms.HiddenInput(attrs={
+		'class': 'form-control',
+	}), initial=0)
+
+	name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Наименование',
+	}))
+
+	customer = forms.ModelChoiceField(queryset=Customer.objects.all(),
+		 widget=forms.Select(attrs={
+		'class': 'form-control',
+		'placeholder': 'Заказчик',
+		'readonly': 'readonly',
+	}))
+
+	position = forms.ModelChoiceField(queryset=Position.objects.all(),
+		 widget=forms.Select(attrs={
+		'class': 'form-control',
+		'placeholder': 'Должность',
+	}))
+
+	person = forms.ModelChoiceField(queryset=Person.objects.all(),
+		 widget=forms.Select(attrs={
+		'class': 'form-control',
+		'placeholder': 'Физ.лицо',
+	}))
+	
+	email = forms.EmailField(max_length=254, required=False, widget=forms.EmailInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'E-mail',
+	}))
+
+	address1 = forms.CharField(max_length=1024, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Адрес',
+	}))
+
+	address2 = forms.CharField(max_length=1024, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Адрес',
+	}))
+
+	phone1 = forms.CharField(max_length=25, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Телефон',
+		}))
+
+	phone2 = forms.CharField(max_length=25, required=False, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Телефон',
+	}))
+
+	description = forms.CharField(max_length=1024, required=False, widget=forms.Textarea(attrs={
+		'class': 'form-control',
+		'rows': '5',
+		'placeholder': 'Описание',
+	}))
+
+	class Meta:
+		model = Employee
+		fields = [
+			'id',
+			'name',
+			'email',
+			'address1',
+			'address2',
+			'phone1',
+			'phone2',
+			'description',
+			]
+
+
 class Task(models.Model):
 
 	date = models.DateTimeField(verbose_name="Дата", auto_now_add=True)
@@ -201,7 +390,10 @@ class Task(models.Model):
 
 
 	def __str__(self):
-		return f'Задача №{self.id} от {self.date.strftime("%d.%m.%Y")}'
+
+		if self.date:
+			return f'Задача №{self.id} от {self.date.strftime("%d.%m.%Y")}'
+		return f'Задача № от '
 
 	def save(self, *args, **kwargs):
 		super(Task, self).save(*args, **kwargs)
@@ -259,6 +451,96 @@ class Task(models.Model):
 		verbose_name = 'Задача'
 		verbose_name_plural = 'Задачи'
 		ordering = ['dead_line']
+
+
+class TaskForm(ModelForm):
+
+	id = forms.IntegerField(widget=forms.HiddenInput(attrs={
+		'class': 'form-control',
+	}), initial=0)
+	
+	customer = forms.ModelChoiceField(queryset=Customer.objects.all(),
+		 widget=forms.Select(attrs={
+		'class': 'form-control',
+		'placeholder': 'Заказчик',
+		'readonly': 'readonly',
+	}), required=True)
+
+	from_customer = forms.ModelChoiceField(queryset=Employee.objects.all(),
+		 widget=forms.Select(attrs={
+		'class': 'form-control',
+		'placeholder': 'От заказчика',
+	}), required=False)
+
+	performer = forms.ModelChoiceField(queryset=Customer.objects.all(),
+		 widget=forms.Select(attrs={
+		'class': 'form-control',
+		'placeholder': 'Исполнитель',
+		'readonly': 'readonly',
+	}), required=True)
+	
+	from_performer = forms.ModelChoiceField(queryset=Employee.objects.all(),
+		 widget=forms.Select(attrs={
+		'class': 'form-control',
+		'placeholder': 'От исполнителя',
+	}), required=False)
+
+	dead_line = forms.DateField(required=True, widget=forms.TextInput(attrs={
+		'class': 'form-control',
+		'type': 'date',
+		'placeholder': 'Исполнить до',
+	}))
+
+	time_scheduled_h = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Время выполнения, факт (час):',
+	}), initial=0)
+
+	time_scheduled_m = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Время выполнения, факт (мин.):',
+	}), initial=0)
+
+	time_actual_h = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Время выполнения, норма (час)',
+	}), initial=0)
+
+	time_actual_m = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={
+		'class': 'form-control',
+		'placeholder': 'Время выполнения, норма (мин.)',
+	}), initial=0)
+
+	description = forms.CharField(max_length=1024, required=False, widget=forms.Textarea(attrs={
+		'class': 'form-control',
+		'rows': '5',
+		'placeholder': 'Описание',
+	}))
+
+	def __init__(self, *args, **kwargs):
+
+		customer = kwargs.pop('customer', None)
+		performer = kwargs.pop('performer', None)
+
+		super(TaskForm, self).__init__(*args, **kwargs)
+		self.fields['from_customer'].queryset = Employee.objects.filter(customer=customer)
+		self.fields['from_performer'].queryset = Employee.objects.filter(customer=performer)
+
+	class Meta:
+		model = Task
+		fields = [
+			'id',
+			'customer',
+			'from_customer',
+			'performer',
+			'from_performer',
+			'dead_line',
+			'time_scheduled_h',
+			'time_scheduled_m',
+			'time_actual_h',
+			'time_actual_m',
+			'description',
+			]
 
 
 class Record(models.Model):#record of status change log
--- crmapp/views.py
+++ crmapp/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import redirect
 from baseapp.core import get_context
 
 from .models import Customer
+from .models import CustomerForm
+
 from .models import Employee
+from .models import EmployeeForm
+
 from .models import Task
+from .models import TaskForm
+
 from .models import Person
 from .models import PersonForm
 
@@ -83,85 +89,91 @@ def send_to_D(request, id):
 
 def save_task(request):
 
+	# if request.user.is_authenticated:
+
+	# 	id 					= int(request.POST.get("id", "0"))
+	# 	customer_id 		= request.POST.get("customer", "0")
+	# 	from_customer_id 	= request.POST.get("from_customer", "0")
+	# 	performer_id 		= request.POST.get("performer", "0")
+	# 	from_performer_id 	= request.POST.get("from_performer", "0")
+		
+	# 	try:
+	# 		dead_line		= datetime.strptime(request.POST.get("dead_line", None), "%Y-%m-%d")
+	# 	except:
+	# 		dead_line 		= None
+
+	# 	description 		= request.POST.get("description", "")
+
+	# 	customer 			= Customer.objects.filter(id=customer_id).first()
+	# 	from_customer 		= Employee.objects.filter(id=from_customer_id).first()
+
+	# 	performer 			= Customer.objects.filter(id=performer_id).first()
+	# 	from_performer 		= Employee.objects.filter(id=from_performer_id).first()
+
+
+	# 	time_scheduled_h 	= int(request.POST.get("time_scheduled_h", "0"))
+	# 	time_scheduled_m 	= int(request.POST.get("time_scheduled_m", "0"))
+	# 	time_actual_h 		= int(request.POST.get("time_actual_h", "0"))
+	# 	time_actual_m 		= int(request.POST.get("time_actual_m", "0"))
+		
+	# 	if id == 0:
+
+	# 		if create_new_task(customer=customer, from_customer=from_customer, performer=performer, from_performer=from_performer, 
+	# 							dead_line=dead_line, description=description, time_scheduled_h=time_scheduled_h, 
+	# 							time_scheduled_m=time_scheduled_m, time_actual_h=time_actual_h, time_actual_m=time_actual_m, user=request.user):
+	# 			return redirect('show-kanban')
+	# 	else:
+
+	# 		if update_task(task=Task.objects.get(id=id), customer=customer, from_customer=from_customer, performer=performer, 
+	# 						from_performer=from_performer, dead_line=dead_line, description=description, time_scheduled_h=time_scheduled_h,
+	# 						time_scheduled_m=time_scheduled_m, time_actual_h=time_actual_h, time_actual_m=time_actual_m, user=request.user):
+	# 			return redirect('show-kanban')
 	if request.user.is_authenticated:
+		if request.method == 'POST':
+			
+			try:
+				task = Task.objects.get(id=int(request.POST.get('id', '0')))
+			except:
+				task = None
 
-		id 					= int(request.POST.get("id", "0"))
-		customer_id 		= request.POST.get("customer", "0")
-		from_customer_id 	= request.POST.get("from_customer", "0")
-		performer_id 		= request.POST.get("performer", "0")
-		from_performer_id 	= request.POST.get("from_performer", "0")
-		
-		try:
-			dead_line		= datetime.strptime(request.POST.get("dead_line", None), "%Y-%m-%d")
-		except:
-			dead_line 		= None
+			print(task)
+			print(request.POST)
+			taskForm = TaskForm(request.POST, instance=task)
+			if taskForm.is_valid():
+				task = taskForm.save(commit=False)
 
-		description 		= request.POST.get("description", "")
+				task.customer = taskForm.cleaned_data['customer']
+				task.from_customer = taskForm.cleaned_data['from_customer']
+				task.performer = taskForm.cleaned_data['performer']
+				task.from_performer = taskForm.cleaned_data['from_performer']
 
-		customer 			= Customer.objects.filter(id=customer_id).first()
-		from_customer 		= Employee.objects.filter(id=from_customer_id).first()
-
-		performer 			= Customer.objects.filter(id=performer_id).first()
-		from_performer 		= Employee.objects.filter(id=from_performer_id).first()
-
-
-		time_scheduled_h 	= int(request.POST.get("time_scheduled_h", "0"))
-		time_scheduled_m 	= int(request.POST.get("time_scheduled_m", "0"))
-		time_actual_h 		= int(request.POST.get("time_actual_h", "0"))
-		time_actual_m 		= int(request.POST.get("time_actual_m", "0"))
-		
-		if id == 0:
-
-			if create_new_task(customer=customer, from_customer=from_customer, performer=performer, from_performer=from_performer, 
-								dead_line=dead_line, description=description, time_scheduled_h=time_scheduled_h, 
-								time_scheduled_m=time_scheduled_m, time_actual_h=time_actual_h, time_actual_m=time_actual_m, user=request.user):
 				return redirect('show-kanban')
-		else:
-
-			if update_task(task=Task.objects.get(id=id), customer=customer, from_customer=from_customer, performer=performer, 
-							from_performer=from_performer, dead_line=dead_line, description=description, time_scheduled_h=time_scheduled_h,
-							time_scheduled_m=time_scheduled_m, time_actual_h=time_actual_h, time_actual_m=time_actual_m, user=request.user):
-				return redirect('show-kanban')
-
 	return redirect(request.META['HTTP_REFERER'])
 	
 
 def show_task(request, id):
 	
-	context 						= get_context()	
-	context['task'] 				= Task.objects.get(id=id)
-	context['customer'] 			= context['task'].customer
-	context['performer'] 			= context['task'].performer
-
-	context['from_customer'] 		= context['task'].from_customer
-	context['customer_employes'] 	= context['customer'].get_employes()
-	context['from_performer'] 		= context['task'].from_performer
-	context['performer_employes'] 	= context['performer'].get_employes()
-	context['dead_line'] 			= context['task'].get_dead_line()
-	context['time_scheduled_h'] 	= context['task'].time_scheduled_h
-	context['time_scheduled_m'] 	= context['task'].time_scheduled_m
-	context['time_actual_h'] 		= context['task'].time_actual_h
-	context['time_actual_m'] 		= context['task'].time_actual_m
-	context['description'] 			= context['task'].description
+	task = Task.objects.get(id=id)
+	
+	context = get_context()	
+	context['task']	= TaskForm(instance=task, 
+								customer=task.customer,
+								performer=task.performer)
 
 	return render(request, "crmapp/task.html", context)
 
 
 def new_task(request, customer_id):
 	
-	context 						= get_context()	
-	context['task'] 				= None	
-	context['customer'] 			= Customer.objects.get(id=customer_id)
-	context['performer'] 			= get_default_performer()
-	context['customer_employes'] 	= context['customer'].get_employes()
-	context['performer_employes'] 	= context['performer'].get_employes()
+	customer = Customer.objects.get(id=customer_id)
 
-	context['from_performer'] 		= get_current_employee(request)
-
-	context['time_scheduled_h'] 	= 0
-	context['time_scheduled_m'] 	= 0
-	context['time_actual_h'] 		= 0
-	context['time_actual_m'] 		= 0
+	context = get_context()		
+	context['task'] = TaskForm(initial = {
+						'customer': customer,
+						'performer': get_default_performer(),
+					}, 
+					customer=customer,
+					performer=get_default_performer())
 
 	return render(request, "crmapp/task.html", context)
 
@@ -169,7 +181,7 @@ def new_task(request, customer_id):
 def show_persons(request):
 	if request.user.is_authenticated:
 		context 				= get_context()
-		context['persons'] 		= Person.objects.all()
+		context['persons'] 		= Person.objects.all()		
 		return render(request, "crmapp/persons.html", context)
 	return redirect('show-auth')
 
@@ -177,56 +189,42 @@ def show_persons(request):
 def new_person(request):
 
 	context 				= get_context()		
-	context['person'] 		= PersonForm()
+	context['person'] 		= PersonForm()	
 	return render(request, "crmapp/person.html", context)
 
 
 def show_person(request, id):
+
 	if request.user.is_authenticated:
-		context 				= get_context()		
-		context['person'] 		= Person.objects.get(id=id)
+
+		context 				= get_context()				
+		context['person'] 		= PersonForm(instance=Person.objects.get(id=id))		
 		return render(request, "crmapp/person.html", context)
 	return redirect('show-auth')
 
 
 def save_person(request):
 
-	id 						= int(request.POST.get('id', "0"))
-	first_name 				= request.POST.get('first_name', '')
-	middle_name 			= request.POST.get('middle_name', '')
-	last_name 				= request.POST.get('last_name', '')
-	
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			
+			try:
+				person = Person.objects.get(id=int(request.POST.get('id', '0')))
+			except:
+				person = None
 
-	try:
-		birthdate 			= datetime.strptime(request.POST.get('birthdate', None), '%Y-%m-%d')
-	except:
-		birthdate			= None
-		
-	sex 					= request.POST.get('sex', '')
-	email 					= request.POST.get('email', '')
-	address1 				= request.POST.get('address1', '')
-	address2 				= request.POST.get('address2', '')
-	phone1 					= request.POST.get('phone1', '')
-	phone2 					= request.POST.get('phone2', '')
-	description 			= request.POST.get('description', '')
-
-	if id == 0:
-
-		if create_new_person(first_name=first_name, middle_name=middle_name, last_name=last_name, birthdate=birthdate, 
-							sex=sex, email=email, address1=address1, address2=address2, phone1=phone1, phone2=phone2, description=description):			
-			return redirect('persons')
-	else:
-
-		if update_person(person=Person.objects.get(id=id), first_name=first_name, middle_name=middle_name, last_name=last_name, 
-						birthdate=birthdate, sex=sex, email=email, address1=address1, address2=address2, phone1=phone1,
-						phone2=phone2, description=description):
-			return redirect('persons')
+			personForm = PersonForm(request.POST, instance=person)
+			if personForm.is_valid():
+				personForm.save()
+				return redirect('persons')
 
 	return redirect(request.META['HTTP_REFERER'])
 
 
 def show_customers(request):
+
 	if request.user.is_authenticated:
+		
 		context = get_context()
 		context['customers'] = Customer.objects.all()
 		return render(request, "crmapp/customers.html", context)
@@ -234,9 +232,11 @@ def show_customers(request):
 
 
 def show_customer(request, id):
+	
 	if request.user.is_authenticated:
+	
 		context = get_context()	
-		context['customer'] = Customer.objects.get(id=id)
+		context['customer'] = CustomerForm(instance=Customer.objects.get(id=id))		
 		return render(request, "crmapp/customer.html", context)
 	return redirect('show-auth')
 
@@ -244,89 +244,80 @@ def show_customer(request, id):
 def new_customer(request):
 
 	context = get_context()	
+	context['customer'] = CustomerForm()
 	return render(request, "crmapp/customer.html", context)
 
 
 def save_customer(request):
-	
-	id 			= int(request.POST.get('id', "0"))
-	name 		= request.POST.get('name', '')
-	full_name 	= request.POST.get('fullName', '')
-	inn 		= request.POST.get('inn', '')
-	kpp 		= request.POST.get('kpp', '')
-	email 		= request.POST.get('email', '')
-	address1 	= request.POST.get('address1', '')
-	address2 	= request.POST.get('address2', '')
-	phone1 		= request.POST.get('phone1', '')
-	phone2 		= request.POST.get('phone2', '')
-	description = request.POST.get('description', '')
-	
-	if id==0:
+		
+	if request.user.is_authenticated:
+		if request.method == 'POST':
 
-		if create_new_customer(name=name, full_name=full_name, inn=inn, kpp=kpp, email=email,
-								address1=address1, address2=address2, phone1=phone1, phone2=phone2, description=description):
-			return redirect('show-crm-index')
-	else:
-		if update_customer(Customer.objects.get(id=id), name=name, full_name=full_name, inn=inn, kpp=kpp, 
-							email=email, address1=address1, address2=address2, phone1=phone1, phone2=phone2, description=description):
-			return redirect('show-crm-index')
+			try:
+				customer = Customer.objects.get(id=int(request.POST.get('id', '0')))
+			except:
+				customer = None
 
+			customerForm = CustomerForm(request.POST, instance=customer)
+			if customerForm.is_valid():
+				customerForm.save()
+				return redirect('customers')
 
 	return redirect(request.META['HTTP_REFERER'])
 
 
 def new_employee(request, customer_id):
 	
-	context = get_context()
-	context['customer'] = Customer.objects.get(id=customer_id)
-	context['customers'] = Customer.objects.all()	
-	context['persons'] = Person.objects.all()
-	context['positions'] = Position.objects.all()
-	return render(request, "crmapp/employee.html", context)
+	if request.user.is_authenticated:
+		
+		context = get_context()
+		context['employee'] = EmployeeForm(initial = {
+					'customer': customer_id,					
+					})
+		return render(request, "crmapp/employee.html", context)
+	return redirect('show-auth')
 
 
 def show_employee(request, id):
+	
 	if request.user.is_authenticated:
+		
+		employee = Employee.objects.get(id=id)
+
 		context = get_context()
-		context['employee'] = Employee.objects.get(id=id)
-		context['customer'] = context['employee'].customer
-		context['customers'] = Customer.objects.all()	
-		context['persons'] = Person.objects.all()
-		context['positions'] = Position.objects.all()
+		context['employee'] = EmployeeForm(instance= employee, 
+					initial = {
+					'customer': employee.customer.pk,
+					'position': employee.position.pk,
+					'person': employee.person.pk,
+					})
+
 		return render(request, "crmapp/employee.html", context)
 	return redirect('show-auth')
 
 
 def save_employee(request):
-	
-	id 				= int(request.POST.get('id', "0"))
-	name 			= request.POST.get('name', '')
+		
+	if request.user.is_authenticated:
+		if request.method == 'POST':
 
-	customer_id 	= request.POST.get('customer_id', '')
-	person_id 		= request.POST.get('person_id', '')
-	position_id 	= request.POST.get('position_id', '')
-	
-	email 			= request.POST.get('email', '')
-	address1 		= request.POST.get('address1', '')
-	address2 		= request.POST.get('address2', '')
-	phone1 			= request.POST.get('phone1', '')
-	phone2 			= request.POST.get('phone2', '')
-	
-	customer 		= Customer.objects.filter(id=customer_id).first()
-	person 			= Person.objects.filter(id=person_id).first()
-	position 		= Position.objects.filter(id=position_id).first()
+			try:
+				employee = Employee.objects.get(id=int(request.POST.get('id', '0')))
+			except:
+				employee = None
 
-	if id==0:
+			employeeForm = EmployeeForm(request.POST, instance=employee)
+			if employeeForm.is_valid():
+				employee = employeeForm.save(commit=False)
 
-		if create_new_employee(name=name, customer=customer, person=person, position=position, email=email,
-			address1=address1, address2=address2, phone1=phone1, phone2=phone2):				
-			pass		
-	else:
-		if update_employee(Employee.objects.get(id=id), name=name, customer=customer, person=person, 
-			position=position, email=email, address1=address1, address2=address2, phone1=phone1, phone2=phone2):			
-			pass	
+				employee.customer = employeeForm.cleaned_data['customer']
+				employee.position = employeeForm.cleaned_data['position']
+				employee.person = employeeForm.cleaned_data['person']
 
-	return redirect('show-customer', id=str(customer_id))
+				employee.save()
+				return redirect('show-customer', id=str(employee.customer.pk))
+
+	return redirect(request.META['HTTP_REFERER'])
 
 
 def show_report_001(request):
