cb5ce1ed718eadbc2ac5d1fb1ca790f6742e7836
MaximPekshev <mp198769@gmail.com>
2021-08-05 09:59:04 +0300

    wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/var/www/u0872810/data/www/annasoft.site/annasoft')
+sys.path.insert(0, '/var/www/u0872810/data/www/annasoft.ru/annasoft')
 sys.path.insert(1, '/var/www/u0872810/data/negrustnoenv/lib/python3.9/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'annasoft.settings'
 from django.core.wsgi import get_wsgi_application
